90316c8e98a282432405fc7fce3cf547800c28d2
Jannatul Mawa Mona <it23029@mbstu.ac.bd>
2024-11-21 18:50:22 +0600

    Add files via upload

---
+++ Exer10Avgspeed.java
@@ -0,0 +1,21 @@
+//compute the average value of a runner
+
+class Exer10Avgspeed
+{
+public static void main(String[]args)
+{
+double time,distance,avg;
+ time=(45.0/60.0)+(30.0/3600.0);
+System.out.println("the avg speed is : "+time);
+
+
+distance=14/1.6;
+System.out.println("the avg speed is : "+distance);
+
+avg=distance/time;
+
+System.out.println("the avg speed is : "+avg);
+
+
+}
+}
